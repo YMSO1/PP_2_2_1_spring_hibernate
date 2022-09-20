3dd08b133c8d3d9bccf281841e84734a8d4d1960
YMSO1 <yms01@ya.ru>
2022-09-20 12:08:01 +0300

    Добавил Car

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,11 +16,16 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      carService.add(new Car("Lexus", 300));
+      carService.add(new Car("BMW", 2055));
+      carService.add(new Car("Toyota", 4997));
+      carService.add(new Car("Nissan", 20));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +33,13 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+      List<Car> cars = carService.listCar();
+      for (Car car : cars) {
+         System.out.println("Id = " + car.getId());
+         System.out.println("Model = " + car.getModel());
+         System.out.println("Series = " + car.getSeries());
          System.out.println();
       }
 
